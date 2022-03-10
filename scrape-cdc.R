b51ad963f407ea857b53080f8942587d43a66649
Michael DeWitt <me.dewitt.jr@gmail.com>
2022-03-10 16:41:28 -0500

    Create scrape-cdc.R
    adding scraper script

---
+++ scrape-cdc.R
@@ -0,0 +1,17 @@
+library(jsonlite)
+
+end <- "/coronavirus/2019-ncov/modules/science/us-community-levels-by-county.json"
+
+slugify_date <- function(x){
+	x <- stringi::stri_replace_all_regex(x,"[^\\P{P}-]","")
+	x <- gsub(x, pattern = " ", replacement = "-")
+	x
+}
+ping_time <- slugify_date(Sys.time())
+
+download.file(
+  paste0("https://www.cdc.gov/", end),
+sprintf("data/%s.json", ping_time),
+quiet = TRUE,
+cacheOK = FALSE
+)
